ab1440b3be6107b6dba0f5460fbe7213a938bb4d
Khaled Maher <maxios.code@gmail.com>
2019-10-06 17:16:51 +0200

    init ORM sequelize

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var express = require('express')
 var app = express()
 
 app.listen(8080, () => console.log(`listenting on port 8080...`)).on('error', console.log);
+
+app.get('/', function(e, r) {
+  r.send('hello world')
+});
