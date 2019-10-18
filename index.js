9bd25696c2e864a3014920b6e5478c2620c0d92a
Khaled Maher <maxios.code@gmail.com>
2019-10-18 11:17:15 +0200

    init and modularize API endpoints

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var express = require('express')
+const express = require('express')
 
-var app = express()
+const routes = require('./api');
 
-app.listen(8080, () => console.log(`listenting on port 8080...`)).on('error', console.log);
+const app = express();
+const PORT = 8080;
 
-app.get('/', function(e, r) {
-  r.send('hello world')
+app.use('/sessions', routes.sessions);
+app.use('/instructors', routes.instructors);
+app.use('/systems', routes.systems);
+app.use('/locations', routes.locations);
+app.use('/sciences', routes.sciences);
+app.use('/tags', routes.tags);
+app.use('/faqs', routes.faqs);
+
+app.listen(PORT, () => console.log(`listenting on port ${PORT}...`)).on('error', console.log);
+
+app.get('/howdy', function(e, r) {
+  r.send('api call successfully responded to you!');
 });
