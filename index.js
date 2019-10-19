a863693e506cea59248afa08c57ced38cd727a70
Khaled Maher <maxios.code@gmail.com>
2019-10-19 21:51:23 +0200

    add api namespace

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const cors = require('cors');
 const routes = require('./api');
 
 const app = express();
-const PORT = 8080;
+const PORT = 8081;
 
 // CORS enabled
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/sessions', routes.sessions);
-app.use('/instructors', routes.instructors);
-app.use('/systems', routes.systems);
-app.use('/locations', routes.locations);
-app.use('/sciences', routes.sciences);
-app.use('/tags', routes.tags);
-app.use('/faqs', routes.faqs);
+app.use('/api/sessions', routes.sessions);
+app.use('/api/instructors', routes.instructors);
+app.use('/api/systems', routes.systems);
+app.use('/api/locations', routes.locations);
+app.use('/api/sciences', routes.sciences);
+app.use('/api/tags', routes.tags);
+app.use('/api/faqs', routes.faqs);
 
 // boot the server
 app.listen(PORT, () => console.log(`listenting on port ${PORT}...`)).on('error', console.log);
