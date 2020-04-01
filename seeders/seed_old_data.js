ec86763334eb4f2d76cb3ec3d364eb8bcdc36a7e
Khaled Maher <maxios.code@gmail.com>
2020-04-01 17:45:49 +0200

    fix no session created because id exist

--- seeders/seed_old_data.js
+++ seeders/seed_old_data.js
@@ -162,7 +162,14 @@ module.exports = {
     await queryInterface.bulkInsert('Systems', mappedSystems(data["Systems"]), {});
     await queryInterface.bulkInsert('Sessions', mappedCourses(data["Courses"]), {});
     await queryInterface.bulkInsert('Instructables', mappedInstructables(filterInstructor(data["Courses"])), {});
-    return queryInterface.bulkInsert('SessionSciences', mappedSessionScience(filterScience(data["Courses"])), {});
+    await queryInterface.bulkInsert('SessionSciences', mappedSessionScience(filterScience(data["Courses"])), {});
+
+    await queryInterface.sequelize.query(`ALTER SEQUENCE "Sessions_id_seq" RESTART WITH 1010;`);
+    await queryInterface.sequelize.query(`ALTER SEQUENCE "SessionSciences_id_seq" RESTART WITH 1010;`);
+    await queryInterface.sequelize.query(`ALTER SEQUENCE "instructables_id_seq" RESTART WITH 1010;`);
+    await queryInterface.sequelize.query(`ALTER SEQUENCE "Systems_id_seq" RESTART WITH 100;`);
+    await queryInterface.sequelize.query(`ALTER SEQUENCE "Locations_id_seq" RESTART WITH 100;`);
+    return queryInterface.sequelize.query(`ALTER SEQUENCE "Sciences_id_seq" RESTART WITH 100;`);
   },
   down: queryInterface => {
     return queryInterface.bulkDelete('sessions', null, {});
