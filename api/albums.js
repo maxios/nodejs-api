4d75e891e987ab087d7dce3b488260326ed4d3d8
Khaled Maher <maxios.code@gmail.com>
2020-04-17 13:34:14 +0200

    add feature get albums photos urls

--- api/albums.js
+++ api/albums.js
@@ -2,15 +2,28 @@ const router = require('express').Router();
 const { lstatSync, readdirSync } = require('fs')
 const { join } = require('path')
 
+const BASE_URL = 'https://sheikhalamoud.com';
+const ALBUM_BASE_PATH = '../sheikhalamoud/dist/sheikh/static/albums';
 const isDirectory = source => lstatSync(source).isDirectory()
 const getDirectories = source =>
   readdirSync(source).map(name => join(source, name)).filter(isDirectory)
 
+const getFiles = source =>
+  readdirSync(source).map(name => join(source, name))
+
 // GET all Records
 router.get('/', (req, res) => {
-  const dirNames = getDirectories('../../sheikhalamoud/dist/sheikh/static/albums/')
+  const dirNames = getDirectories(ALBUM_BASE_PATH)
   res.json(dirNames);
 });
+
+router.get('/:album_name', (req, res) => {
+  const { album_name } = req.params;
+  const files = getFiles(`${ALBUM_BASE_PATH}/${album_name}`)
+    .map(file => `https://${BASE_URL}/static/albums/${album_name}/${file.split('/').pop()}`);
+
+  res.json(files)
+})
 
 module.exports = router;
 
