7523dc14b01665c4e4a9f36001b6e8e0789f059b
Khaled Maher <maxios.code@gmail.com>
2020-04-17 13:51:02 +0200

    albums endpoints clean code

--- api/albums.js
+++ api/albums.js
@@ -4,25 +4,34 @@ const { join } = require('path')
 
 const BASE_URL = 'https://sheikhalamoud.com';
 const ALBUM_BASE_PATH = '../sheikhalamoud/dist/sheikh/static/albums';
+
 const isDirectory = source => lstatSync(source).isDirectory()
-const getDirectories = source =>
-  readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+const getFiles = source => readdirSync(source).map(name => join(source, name))
+const getLastInUri = uri => uri.split('/').pop();
 
-const getFiles = source =>
-  readdirSync(source).map(name => join(source, name))
 
-// GET all Records
+const albums = getDirectories(ALBUM_BASE_PATH).map(getLastInUri);
+const photos = album_name => getFiles(`${ALBUM_BASE_PATH}/${album_name}`)
+  .map(file => `${BASE_URL}/static/albums/${album_name}/${file.split('/').pop()}`);
+
+// get albums names
 router.get('/', (req, res) => {
-  const dirNames = getDirectories(ALBUM_BASE_PATH)
-  res.json(dirNames);
+  res.json(albums);
 });
 
+// Get all Photos
+router.get('/photos', (req, res) => {
+  const allPhotos = albums.map(photos).flat();
+
+  res.json(allPhotos);
+})
+
+// Get photos in one album
 router.get('/:album_name', (req, res) => {
   const { album_name } = req.params;
-  const files = getFiles(`${ALBUM_BASE_PATH}/${album_name}`)
-    .map(file => `https://${BASE_URL}/static/albums/${album_name}/${file.split('/').pop()}`);
 
-  res.json(files)
+  res.json(photos(album_name))
 })
 
 module.exports = router;
