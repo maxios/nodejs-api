e233035225cc66880b527d3ca2b8bade900c6214
Khaled Maher <maxios.code@gmail.com>
2020-04-21 20:52:06 +0200

    fix preview url in albums endpoint

--- api/albums.js
+++ api/albums.js
@@ -4,6 +4,7 @@ const { lstatSync, readdirSync, statSync } = require('fs')
 const { join } = require('path')
 
 const BASE_URL = 'https://sheikhalamoud.org';
+const ALBUMS_URL = `${BASE_URL}/static/albums`;
 const ALBUM_BASE_PATH = '../sheikhalamoud/dist/sheikh/static/albums';
 
 const isDirectory = source => lstatSync(source).isDirectory()
@@ -14,7 +15,7 @@ const getLastInUri = uri => uri.split('/').pop();
 
 const albums = getDirectories(ALBUM_BASE_PATH).map(getLastInUri);
 const photos = album_name => getFiles(`${ALBUM_BASE_PATH}/${album_name}`)
-  .map(file => `${BASE_URL}/static/albums/${album_name}/${file.split('/').pop()}`);
+  .map(file => `${ALBUMS_URL}/${album_name}/${file.split('/').pop()}`);
 
 /**
  * Get latest photo from the album and return its URL
@@ -25,7 +26,7 @@ const getPreviewPhoto = album_name => {
   const photosStats = photos_names.map(photo_name => ({name: photo_name, stat: statSync(`${ALBUM_BASE_PATH}/${album_name}/${photo_name}`)}))
   const sortedPhotos = R.sortBy(R.path(['stat', 'ctime']))(photosStats)
 
-  return `${BASE_URL}/${album_name}/${sortedPhotos[0].name}`;
+  return `${ALBUMS_URL}/${album_name}/${sortedPhotos[0].name}`;
 }
 
 // get albums names and latest image preview
